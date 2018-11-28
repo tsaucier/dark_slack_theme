f1392650ae928f6499965ce6c15651905558dfbc
tauri-it <42674308+tauri-it@users.noreply.github.com>
2018-11-27 19:35:07 -0500

    Create SlackDark.ps1

---
+++ SlackDark.ps1
@@ -0,0 +1,28 @@
+$VersionPath = "C:\Users\$env:UserName\AppData\Local\slack\app-*"
+$JsPath = "\resources\app.asar.unpacked\src\static\ssb-interop.js"
+$PathContent = Get-ChildItem $VersionPath -Recurse | Where-Object { $_.PSIsContainer } | Sort-Object LastWriteTime | Select-Object -last 1
+$FullPath = "$($PathContent)" + "$($JsPath)"
+$FileExists = Test-Path $FullPath
+$NightModeId = "slack-night-mode"
+$SlackDarkTheme = @'
+
+document.addEventListener('DOMContentLoaded', function() {
+ $.ajax({
+   url: 'https://raw.githubusercontent.com/laCour/slack-night-mode/master/css/raw/black.css',
+   success: function(css) {
+     $("<style></style>").appendTo('head').html(css);
+   }
+ });
+}); 
+'@
+$ChkSlackDarkTheme = Get-Content $FullPath | Select-String -Pattern $NightModeId
+
+if ($FileExists) {
+    if (!($ChkSlackDarkTheme)) {
+    Add-Content -Path $FullPath -Value $SlackDarkTheme
+  }
+}
+
+else {
+    Write-Output "Slack is already using the dark theme"
+}
